206bc17a4f441c3da91a8bd2679d7aa23e5e9272
jiwon010330 <69471733+jiwon010330@users.noreply.github.com>
2020-08-11 20:59:01 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p> 한지원! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
